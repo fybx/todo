ff291c7db091f9dd0c3a69af78f639544e5cc7fa
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-11-13 00:08:39 +0300

    Updated TodoItem to fit XML serialization rules

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,20 +1,20 @@
 using System;
+using System.Xml.Serialization;
 
 namespace todo.Models
 {
     public class TodoItem
     {
+        [XmlElement(Order = 1, ElementName = "Index")]
         public int Index;
+        
+        [XmlElement(Order = 2, ElementName = "Creation")]
         public DateTime Creation;
+        
+        [XmlElement(Order = 3, ElementName = "Completed")]
         public bool Completed;
+        
+        [XmlElement(Order = 4, ElementName = "Description")]
         public string Description;
-
-        public TodoItem(string description, int index)
-        {
-            Index = index;
-            Creation = DateTime.UtcNow;
-            Completed = false;
-            Description = description;
-        }
     }
 }
