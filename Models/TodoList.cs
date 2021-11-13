fff13f9e2dedbff0acdc9a8ca81632f22fc25206
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-11-13 09:57:17 +0300

    Updated EditItem(): Remove item without passing anything to parameter of type object, parameter

--- Models/TodoList.cs
+++ Models/TodoList.cs
@@ -18,11 +18,11 @@ namespace todo.Models
 
         public void AddItem(string description) => Items.Add(new TodoItem {Completed = false, Creation = DateTime.UtcNow, Description = description, Index = Count});
 
-        public void EditItem(int index, Operation operation, object parameter)
+        public void EditItem(int index, Operation operation, object parameter = null)
         {
             if (index < 0 || index >= Items.Count)
                 throw new ArgumentOutOfRangeException(nameof(index));
-            if (parameter is null)
+            if (operation is not Operation.Remove && parameter is null)
                 throw new ArgumentNullException(nameof(parameter));
                 
             if (operation == Operation.Remove)
